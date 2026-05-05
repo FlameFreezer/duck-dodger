5132227f94d0f91ea5f51bf8ec29a5124f129ede
FFreeze <quincymon@icloud.com>
2026-05-04 21:43:28 -0700

    Credits

--- src/Scenes/title.js
+++ src/Scenes/title.js
@@ -11,8 +11,14 @@ class Title extends Phaser.Scene {
     }
     create() {
         this.keys.enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
-        this.text = this.add.bitmapText(canvasW / 2, canvasH / 2, "daydream_3", "press enter to play", 24).setOrigin(0.5);
-        this.text.setBlendMode(Phaser.BlendModes.ADD);
+        this.text = this.add.bitmapText(canvasW / 2, canvasH / 2, "daydream_3", "press enter to play", 24)
+            .setOrigin(0.5)
+            .setBlendMode(Phaser.BlendModes.ADD);
+
+        this.creditTxt = this.add.bitmapText(canvasW - canvasW / 7, canvasH - canvasH / 32, "daydream_3", "By Quincy Hurst", 14)
+            .setOrigin(0.5)
+            .setBlendMode(Phaser.BlendModes.ADD);
+
         //Text flashing: outer event shows text, inner event hides text
         this.textFlashTimer = this.time.addEvent({
             delay: 1500,
