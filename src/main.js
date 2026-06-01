616221f593a57a62f605eb8f97bf344b486d34f9
FFreeze <quincymon@icloud.com>
2026-06-01 15:37:36 -0700

    Cleaned up main

--- src/main.js
+++ src/main.js
@@ -4,15 +4,8 @@
 let config = {
     parent: 'phaser-game',
     type: Phaser.WEBGL,
-    render: {
-        pixelArt: false  
-    },
     scale: {
-        //mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    physics: {
-        default: "arcade"
     },
     backgroundColor: '#0c404c',
     width: canvasW,
